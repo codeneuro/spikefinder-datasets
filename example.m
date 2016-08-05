93a3539e05e555956e094d99755f1eea29e3e246
pberens <pberens>
2016-08-05 13:12:08 +0200

    added matlab example

--- example.m
+++ example.m
@@ -1,4 +1,30 @@
 % example matlab script for loading spikefinder data
 %
 % for more info see https://github.com/codeneuro/spikefinder
-%
+
+
+% load dataset
+dataset = '1';
+calcium_train = csvread([dataset '.train.calcium.csv']);
+spike_train = xlsread([dataset '.train.spikes.csv']);
+
+% plot example neuron
+figure
+n = 5;         % index of neuron
+t = (0:length(calcium_train(:,n))-1)/100;     % 100Hz sampling rate, each bin 10 ms
+plot(t,zscore(calcium_train(:,n)),'k')
+hold on
+plot(t,spike_train(:,5)-2,'r')
+
+xlim([t(1) 400])
+ylim([-2.5 7])
+
+xlabel('Time (s)')
+ylabel('Fluorescence / Spike rate')
+
+legend('calcium','spikes')
+
+% saving predictions
+spike_pred = calcium_train;     % fill spike_pred with your own predictions
+csvwrite([dataset '.train.pred.csv'], spike_pred);
+
